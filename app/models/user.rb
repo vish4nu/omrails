717d1cc74cda68c050cdfd7680692def682e7498
Vishnu Parthasarathy <vish4nu@gmail.com>
2013-07-26 16:53:43 -0700

    removed recoverable password link from user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, #:recoverable,
+          :rememberable, :trackable, :validatable
   # attr_accessible :title, :body
 	attr_accessible :email, :password, :password_confirmation, :name,
 	:remember_me, :provider, :uid, :as => [:default, :admin]
