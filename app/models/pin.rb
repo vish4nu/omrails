35467c27209c4a55726e21ae7354aeb6585c94b1
Vishnu Parthasarathy <vish4nu@gmail.com>
2013-08-08 13:00:35 -0700

    ImageUploadViaUrl

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,5 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description, :image
+  attr_accessible :description, :image, :image_remote_url
 
   belongs_to :user
   has_attached_file :image, styles: { medium: "300x240>" }
@@ -9,6 +9,11 @@ class Pin < ActiveRecord::Base
   validates_attachment :image, presence: true, 
   						content_type: { content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']},
   						size: { less_than: 5.megabytes }
+
+  def image_remote_url=(url_value)
+  	self.image = URI.parse(url_value) unless url_value.blank?
+  	super
+  end
 
 
 end
